8ded273f42a872919e09c9424cc76c909c96517d
vspecii3112 <kennyklleung@gmail.com>
2018-06-21 12:51:45 -0400

    some changes

--- server/config/passport.js
+++ server/config/passport.js
@@ -22,7 +22,6 @@ module.exports = function(passport) {
     }));
 }
 */
-
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../model/user');
--- server/model/user.js
+++ server/model/user.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
-
-
     /*
 
     */
